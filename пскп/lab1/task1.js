50ed88ab158b693c68fd813ff9bd7799aad74bd5
sakuraskip <rainzape@gmail.com>
2025-09-01 16:58:45 +0300

    ПСКП: lab 1 done

---
+++ пскп/lab1/task1.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 8000;
+const server = http.createServer((request,response)=>
+{
+    response.statusCode = 200;
+    response.setHeader('Content-Type','text/html');
+    response.end('<h1>helo world</h1> ');
+})
+
+server.listen(port,host);
