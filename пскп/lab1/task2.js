9144c863c2fec599376eb104ca0aacac383e4f77
unknown <rainzape@gmail.com>
2025-09-07 21:23:21 +0300

    ПСКП: lab1 fix lab3 4/5

--- пскп/lab1/task2.js
+++ пскп/lab1/task2.js
@@ -4,11 +4,23 @@ const host = '127.0.0.1';
 const port = 8000;
 const server = http.createServer((request,response)=>
 {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/html');
-    response.end(`<h1>method : ${request.method}</h1>
+    let body = '';
+    request.on('data',chunk=>
+    {
+        body+= chunk.toString();
+    }
+    );
+    request.on('end',()=>
+    {
+        response.statusCode = 200;
+        response.setHeader('Content-Type','text/html');
+        response.end(`<h1>method : ${request.method}</h1>
         <h2>uri : ${request.url}
-        <h3>http version : ${request.httpVersion}`);
+        <h3>http version : ${request.httpVersion}
+        <h3> request body: ${body},</h3>`);
+        
+    })
+    
 })
 
 server.listen(port,host);
